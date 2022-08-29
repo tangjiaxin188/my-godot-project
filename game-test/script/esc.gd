b99f09f7d37349391f375f73d914e5143ae20638
tangjiaxin188 <111874512+tangjiaxin188@users.noreply.github.com>
2022-08-29 21:29:15 +0800

    Add files via upload

--- game-test/script/esc.gd
+++ game-test/script/esc.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready():
+	Input.set_mouse_mode(2)
 func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(0)
 		get_tree().change_scene("res://scene/2.tscn")
